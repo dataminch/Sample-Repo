53b02e2dc458a8a10915dd949cf781e74a915cce
dataminch <wgpater@yahoo.com>
2022-08-08 12:31:50 -0700

    initial commit

---
+++ script.py
@@ -0,0 +1,21 @@
+import sys
+
+# import os
+# import math
+import requests
+
+# use sys.executable for python path
+# create virtual environment
+# python -m venv venv
+# pip install requests
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
